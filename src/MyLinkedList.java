e1d0f53d98d125501bf5b0e449da600e9ca624a8
Jason Chan <jchan122490@gmail.com>
2016-08-26 08:54:06 -0400

    was easy to do once making the method works lol

---
+++ src/MyLinkedList.java
@@ -0,0 +1,113 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class MyLinkedList<T> implements Iterable<T> {
+
+    private Node<T> head;
+    private int size = 0;
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean remove(int index) {
+        //to-do
+        if (index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        else if (index == 0) {
+            head = head.getNext();
+        }
+        else {
+            Node<T> previousNode = head;
+            Node<T> currentNode = head;
+            for (int i = 0; i < index; i++) {
+                previousNode = currentNode;
+                currentNode = currentNode.getNext();
+            }
+            if (currentNode.getNext() == null) {
+                previousNode.setNext(null);
+            }
+            else {
+                previousNode.setNext(currentNode.getNext());
+            }
+        }
+
+        size--;
+        return true;
+    }
+
+    public T get(int index) {
+        //to-do
+        if (index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        else {
+            Node<T> currentNode = head;
+            while (currentNode.getNext() != null) {
+                currentNode = currentNode.getNext();
+            }
+            return currentNode.getData();
+        }
+    }
+
+    public void add(T obj) {
+        //to-do
+        if (size == 0) {
+            head = new Node<T>(obj, null);
+        }
+        else {
+            Node<T> currentNode = head;
+            for (int i = 0; i < size; i++) {
+                if (currentNode.getNext() != null) {
+                    currentNode = currentNode.getNext();
+                }
+                else {
+                    currentNode.setNext(new Node<T>(obj, null));
+                }
+            }
+        }
+        size++;
+    }
+
+    //The methods below are bonus
+
+    public void add(int index, T obj) {
+        //to-do
+        size++;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        // TODO Auto-generated method stub
+        return new MyIterator<T>(head);
+    }
+
+    private class MyIterator<T> implements Iterator<T> {
+
+        private Node<T> node;
+
+        public MyIterator(Node<T> node) {
+            //to-do
+        }
+
+        @Override
+        public boolean hasNext() {
+            //to-do
+            return true;
+        }
+
+        @Override
+        public T next() {
+            //to-do
+            return null;
+        }
+
+        @Override
+        public void remove() {
+            // TODO Auto-generated method stub
+
+        }
+
+    }
+}
